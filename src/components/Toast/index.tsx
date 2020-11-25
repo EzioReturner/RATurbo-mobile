630b4c6926106ae3edb63d403339de21bf40906d
ezio <ezioreturner@gmail.com>
2020-11-25 17:47:00 +0800

    update toast

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Toast } from 'antd-mobile';
-import { InfoCircleOutlined } from '@ant-design/icons';
+import { InfoCircleOutlined, CheckCircleOutlined } from '@ant-design/icons';
+import './index.less';
 
-const RAToast = (text: any, duration?: number) => {
+/**
+ * 提示组件
+ * @param {text} 提示文字
+ * @param {type} toast类型
+ * @param {duration} 存在时间
+ */
+
+
+const RAToast = (text: any, type: 'warning' | 'success', duration?: number) => {
   return Toast.info(
-    <div className="limit-info">
-      <InfoCircleOutlined className="limit-info-icon" />
-      <span className="limit-info-text">{text}</span>
+    <div className={`toast-info info-${type}`}>
+      {type === 'success' ? (
+        <CheckCircleOutlined className="toast-info-icon" />
+      ) : (
+        <InfoCircleOutlined className="toast-info-icon" />
+      )}
+      <span className="toast-info-text">{text}</span>
     </div>,
     duration || 2
   );
